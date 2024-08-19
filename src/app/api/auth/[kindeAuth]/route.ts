c30443ee4616ed0dcbe77cc54608deb879681765
Ciptadi <chipz.ciptadi@gmail.com>
2024-08-19 21:32:44 +0700

    added options for preflight

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,2 +1,9 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextResponse } from "next/server";
 export const GET = handleAuth();
+
+export const OPTIONS = async () => {
+  return new NextResponse("", {
+    status: 200,
+  });
+};
