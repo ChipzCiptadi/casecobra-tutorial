32d658a882d8bb7596b9dd90f775e9da2daa4a1f
Ciptadi <chipz.ciptadi@gmail.com>
2024-08-19 21:39:29 +0700

    middleware.ts

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,9 +1,3 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-import { NextResponse } from "next/server";
-export const GET = handleAuth();
 
-export const OPTIONS = async () => {
-  return new NextResponse("", {
-    status: 200,
-  });
-};
+export const GET = handleAuth();
